404fef42df0a655206f70d4ca087733a9bae0b32
Ashar Alvany <ashar@aleksomega.com>
2023-01-28 17:56:00 -0600

    initial

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-const { Hmac } = require("crypto");
 let fs = require("fs");
 
 class hashMap {
   constructor(linesOfFile) {
+    this.startingNumOfLines = linesOfFile.length;
     this.linesOfFile = linesOfFile;
     this.numOfLines = linesOfFile.length;
     this.occupied = 0;
     this.hashSize = this.getHashSize(linesOfFile);
-    console.log(this.hashSize)
+    console.log(this.hashSize);
     this.hashedArray = Array(this.hashSize);
     this.hashArray(linesOfFile);
   }
@@ -38,65 +38,82 @@ class hashMap {
     let zipCode = parseInt(line.slice(0, 5));
     let hashPosition = zipCode % this.hashSize;
     while (this.hashedArray[hashPosition] != null) {
-        hashPosition = (hashPosition + 1) % this.hashSize;
+      hashPosition = (hashPosition + 1) % this.hashSize;
     }
     this.hashedArray[hashPosition] = line;
     this.occupied++;
-    if(this.occupied >= 2 * this.numOfLines){
-        this.hashMapResize()
+    if (this.occupied >= 2 * this.startingNumOfLines) {
+      this.hashMapResize();
     }
   }
-  hashMapResize(){
-    let tempArray = []
+  hashMapResize() {
+    console.log("resizing...");
+    let tempArray = [];
     this.hashedArray.forEach((element) => {
-        if(element != null) {
-            tempArray.push(element);
-        }
+      if (element != null) {
+        tempArray.push(element);
+      }
     });
-    //finish this
+    let tempHashMap = new hashMap(tempArray);
+    this.hashedArray = tempHashMap.hashedArray;
+    this.occupied = tempHashMap.occupied;
+    this.hashSize = tempHashMap.hashSize;
   }
-  add(zipCode, data){
-    let line = `${zipCode} - ${data}`;
-    this.linesOfFile++;
-    console.log(this.linesOfFile);
+  add(zipCode, data) {
+    let line = `${zipCode} - ${JSON.stringify(data)}`;
+    this.numOfLines++;
+    fs.appendFile("mlsProperties.txt", `\n${line}`, (err) => {
+      console.log("Updating file...")
+      if (err) {
+        console.log(err);
+      }
+    });
     this.hash(line);
   }
-  find(zipCode){
-    let zipCode = parseInt(zipCode);
+  find(zipCode) {
+    zipCode = parseInt(zipCode);
     let found = false;
     let index = zipCode % this.hashSize;
-    while(!found){
-        if(this.hashedArray[index].slice(0,5) == zipCode.toString()){
-            found == true;
-            break;
+    console.log(`To find: ${zipCode}`);
+    console.log(`At index: ${index}`);
+    console.log(`Against: ${this.hashedArray[index].slice(0, 5)}`);
+    while (!found) {
+      if (this.hashedArray[index].slice(0, 5) == zipCode.toString()) {
+        console.log("Found!");
+        found = true;
+        break;
+      } else {
+        if (this.hashedArray[index] == null) {
+          break;
         }
-        else{
-            if(this.hashedArray[index] == null){
-                break;
-            }
-            index++;
-        }
+        index++;
+      }
     }
-    if(found){
-        return index;
-    }
-    else{
-        return -1;
+    if (found) {
+      return index;
+    } else {
+      return -1;
     }
   }
-  print(){
+  print() {
     console.log(
-        `Lines of File: ${this.numOfLines}\n
+      `Lines of File: ${this.numOfLines}\n
         Size of Array: ${this.hashSize}\n
         Occupied: ${this.occupied}\n
-        Array Elements: `, this.hashedArray
-    )
+        Array Elements: `,
+      this.hashedArray
+    );
   }
 }
 
 // create hashmap of file
 fs.readFile("mlsProperties.txt", (err, data) => {
-  linesOfFile = data.toString().split("\n");
-  let hm = new hashMap(linesOfFile);
-  hm.print();
+  if (data != "") {
+    linesOfFile = data.toString().split("\n");
+    // console.log(linesOfFile)
+    let hm = new hashMap(linesOfFile);
+    hm.print();
+    // console.log()
+    console.log(hm.find(75052));
+  }
 });
